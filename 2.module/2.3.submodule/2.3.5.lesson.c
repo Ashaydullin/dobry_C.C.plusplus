7ed78076c4f3d6c805b649527b4f828e5bb17657
aleksandr.shaydullin <aleksandr.shaydullin@lamoda.ru>
2024-07-07 13:39:22 +0400

    добавил главу 2.3 и решение заданий

---
+++ 2.module/2.3.submodule/2.3.5.lesson.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int value = getchar();
+    putchar(value);// здесь продолжайте программу
+
+    return 0;
+}
---
+++ 2.module/2.3.submodule/2.3.7.lesson.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int value = getchar();
+    int value1 = getchar();
+    int value2 = getchar();
+    putchar(value2);
+    putchar(value1);
+    putchar(value);// здесь продолжайте программу
+
+    return 0;
+}
