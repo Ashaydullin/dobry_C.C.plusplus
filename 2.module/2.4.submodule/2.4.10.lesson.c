6504a5cccd34ebc99042ee932566d4820a2760d5
aleksandr.shaydullin <aleksandr.shaydullin@lamoda.ru>
2024-07-07 22:24:21 +0400

    feat: добавил конспект и решения по модулю 2.4

---
+++ 2.module/2.4.submodule/2.4.10.lesson.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double d = 546e-3;
+
+    printf("%.3f", d);// здесь продолжайте программу
+
+    return 0;
+}
---
+++ 2.module/2.4.submodule/2.4.4.lesson.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char ch = 'u';
+    short sh = -55;
+    int var_i = 1024;
+    double var_d = 3.1415;
+    printf("%c %+d %d %f\n", ch, sh, var_i, var_d);
+
+    // здесь продолжайте программу
+
+    return 0;
+}
